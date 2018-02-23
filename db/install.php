a0e9815d2f6f7fcfb948236706c8081991cb369e
ehuamanip <ehuamanip@pucp.pe>
2018-02-23 11:15:17 -0500

    nuevo evento para nuevos usuarios

--- db/install.php
+++ db/install.php
@@ -5,10 +5,11 @@ function xmldb_block_chat_install()
     global $DB, $USER;
     //listamos los usuarios activos de la plataforma
     $users = $DB->get_records('user',['deleted'=>0,'suspended'=>0]);
-    $objBlockInstanceExist =  $DB->get_record('block_instances',['parentcontextid'=>$context_user->id], '*', IGNORE_MULTIPLE);
+    
     if(is_array($users) && count($users)>0){
         foreach($users as $index=>$objUser){
             $context_user = context_user::instance($objUser->id);
+            $objBlockInstanceExist =  $DB->get_record('block_instances',['parentcontextid'=>$context_user->id], '*', IGNORE_MULTIPLE);
             //validamos si tiene un registro de usuario
             $objBlockInstance =  $DB->get_record('block_instances',['blockname'=>'chat','parentcontextid'=>$context_user->id]);
             if(!is_object($objBlockInstance)){
